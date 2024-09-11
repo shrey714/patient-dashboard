c24ecead32d18685961dfb3616b9dfff9829bef3
Oza Jeetkumar Vishnubhai <onlyjeet31@gmail.com>
2024-09-12 03:49:17 +0530

    phoneemail authentivation

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,98 +1,35 @@
 "use client";
-import React, { useState } from 'react';
-import {auth} from "../../firebase/firebaseConfig"
-import { getAuth,RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-import { Button, Card, CardContent, TextField, Typography } from '@mui/material';
 
-
+import React, { useEffect } from 'react';
 declare global {
-    interface Window {
-        confirmationResult:any;
-    }
-}
-
-
-const App = () => {
-
-  const [phone, setPhone] = useState('+91');
-  const [hasFilled, setHasFilled] = useState(false);
-  const [otp, setOtp] = useState('');
-
-  const generateRecaptcha = () => {
-    window.recaptchaVerifier = new RecaptchaVerifier('recaptcha', {
-      'size': 'invisible',
-      'callback': () => {
-        // reCAPTCHA solved, allow signInWithPhoneNumber.
-        // ...
-      }
-    },auth);
-  }
-
-  const handleSend = (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-    setHasFilled(true);
-    generateRecaptcha();
-    let appVerifier = window.recaptchaVerifier;
-    signInWithPhoneNumber(auth, phone, appVerifier)
-      .then((confirmationResult: any) => {
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
-        window.confirmationResult = confirmationResult;
-      }).catch((error: any) => {
-        // Error; SMS not sent
-        console.log(error);
-      });
-  }
-  
-  const verifyOtp = (event: { target: { value: any; }; }) => {
-    let otp = event.target.value;
-    setOtp(otp);
-
-    if (otp.length === 6) {
-      // verifu otp
-      let confirmationResult = window.confirmationResult;
-      confirmationResult.confirm(otp).then((result: { user: any; }) => {
-        // User signed in successfully.
-        let user = result.user;
-        console.log(user);
-        alert('User signed in successfully');
-        // ...
-      }).catch(() => {
-        // User couldn't sign in (bad verification code?)
-        // ...
-        alert('User couldn\'t sign in (bad verification code?)');
-      });
-    }
-  }
-
-  if(!hasFilled){
-    return (
-      <div className='app__container'>
-        <Card sx={{ width: '300px'}}>
-          <CardContent sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-            <Typography sx={{ padding: '20px'}} variant='h5' component='div'>Enter your phone number</Typography>
-            <form onSubmit={handleSend}>
-              <TextField sx={{ width: '240px'}} variant='outlined' autoComplete='off' label='Phone Number' value={phone} onChange={(event) => setPhone(event.target.value)} />
-              <Button type='submit' variant='contained' sx={{ width: '240px', marginTop: '20px'}}>Send Code</Button>
-            </form>
-          </CardContent>
-        </Card>
-        <div id="recaptcha"></div>
-      </div>
-    ) 
-  } else {
-    return (
-      <div className='app__container'>
-        <Card sx={{ width: '300px'}}>
-          <CardContent sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-            <Typography sx={{ padding: '20px'}} variant='h5' component='div'>Enter the OTP</Typography>
-              <TextField sx={{ width: '240px'}} variant='outlined' label='OTP ' value={otp} onChange={verifyOtp} />
-          </CardContent>
-        </Card>
-        <div id="recaptcha"></div>
-      </div>
-    )
+  interface Window {
+    phoneEmailListener:any;
   }
 }
+const SignInButton = () => {
+    useEffect(() => {
+        // Load the external script
+        const script = document.createElement('script');
+        script.src = "https://www.phone.email/sign_in_button_v1.js";
+        script.async = true;
+        document.querySelector('.pe_signin_button')?.appendChild(script);
 
-export default App;
+        // Define the listener function
+        window.phoneEmailListener = function(userObj: { user_json_url: any; }) {
+            const user_json_url = userObj.user_json_url;
+            // Insert the debug message
+            document.querySelector('.pe_signin_button')?.insertAdjacentHTML('beforeend', `<span>Phone Verification Successful !! <br />Read the following user_json_url from the backend to get the verified phone number - ${user_json_url} <br /> Please delete this debug message code from the phoneEmailListener function once you implement integration step 2.</span>`);
+        };
+
+        return () => {
+            // Cleanup the listener function when the component unmounts
+            window.phoneEmailListener = null;
+        };
+    }, []);
+
+    return (
+        <div className="pe_signin_button" data-client-id="17697786150058695225"></div>
+    );
+};
+
+export default SignInButton;
